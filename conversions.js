fc606bffe6900bb3a054283be28ac2aae947d783
Armored Dragon <publicmail@armoreddragon.com>
2026-04-12 09:30:52 -0500

    Improved interface.

--- conversions.js
+++
@@ -1,4 +0,0 @@
-function sizeToMebibyte(value, includeLabel = true) {
-	const MEBIBYTE_SIZE = 1024 ^ 2;
-	return String((value / (MEBIBYTE_SIZE * 1000)).toFixed(2)) + (includeLabel ? " MiB" : "");
-}
